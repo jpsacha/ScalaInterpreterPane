cc6358d32d35dff5d4286d93fd72a6bc243b4946
Sciss <contact@sciss.de>
2010-08-28 12:13:07 +0100

    run.sh update using managed lib and 2.8.0 final paths

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp target/scala_2.8.0.RC1/scalainterpreterpane_2.8.0.RC1-0.13.jar:lib/jsyntaxpane.jar:${SCALA_HOME}/lib/scala-library.jar:${SCALA_HOME}/lib/scala-compiler.jar de.sciss.scalainterpreter.Main
+java -cp target/scala_2.8.0/scalainterpreterpane_2.8.0-0.16.jar:lib_managed/scala_2.8.0/compile/jsyntaxpane-0.9.5-b29.jar:${SCALA_HOME}/lib/scala-library.jar:${SCALA_HOME}/lib/scala-compiler.jar de.sciss.scalainterpreter.Main
